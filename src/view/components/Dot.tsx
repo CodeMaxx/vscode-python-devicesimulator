2406bc626727bc2d0651837b3a42787d59be046d
Andrea Mah <t-anmah@microsoft.com>
2020-01-08 16:08:39 -0800

    minor tests 2

---
+++ src/view/components/Dot.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import "../styles/Dot.css";
+
+class Dot extends React.Component {
+    constructor(props: any) {
+      super(props);
+    }
+  
+    public render() {
+      return (
+        <div className="dot"></div>
+      );
+    }
+  }
+
+  
+export default Dot;
